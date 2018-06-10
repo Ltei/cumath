bed6dc78a705060b91cb1c487d3de5b77a8700db
Ltei <xavier.peltier@outlook.com>
2018-06-10 20:19:43 +0200

    if CUDA_HOME is defined, the build now use it's value instead of the default cuda path

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 
 extern crate cc;
+use std::env;
 
 
 fn main() {
-    println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64");
+
+    if let Ok(cuda_path) = env::var("CUDA_HOME") {
+        println!("cargo:rustc-link-search=native={}/lib64", cuda_path);
+    } else {
+        println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64");
+    }
+
     println!("cargo:rustc-link-lib=dylib=cuda");
     println!("cargo:rustc-link-lib=dylib=cudart");
     println!("cargo:rustc-link-lib=dylib=cublas");
